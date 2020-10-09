8b75924ad0cf7621af8591a35898266c9c2cb772
Antonio Vallez <nekotony@outlook.com>
2020-10-09 15:39:23 -0400

    I have done a template for verification checking

--- run.py
+++ run.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, jsonify, url_for, Markup
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    if_person, if_test, test_type = [request.args.get("if_person"), request.args.get("if_test"), request.args.get("type")]
+    
+    html = '<form action="/check" method="POST"><input type="submit" value="Submit"></form>'
+    return render_template('home.html', html=Markup(html), if_person=if_person, if_test=if_test, test_type=test_type)
 
-if __name__ == '__main__':
+@app.route("/check", methods=["POST"])
+def check():
+    if_person = True #Saved for If_person check function
+    if_test = True #Saved for test regarding face is true
+    test_type = "smiling"
+
+    if [if_person, if_test] == [True, True]:
+        return redirect(url_for(".checkout"))
+    elif [if_person, if_test] == [False, False]:
+        return redirect(url_for(".home", if_person="False", if_test="False", type=test_type))
+    elif if_person is False:
+        return redirect(url_for(".home", if_person="False", if_test="True", type=test_type))
+    elif if_test is False:
+        return redirect(url_for(".home", if_person="True", if_test="False", type=test_type))
+    else:
+        return redirect(url_for(".home", error="hmm"))
+
+@app.route("/checkout")
+def checkout():
+    return "Template for verification is successful"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
