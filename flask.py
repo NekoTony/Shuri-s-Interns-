ce7493b6f0b8e03a27240b99b7fe85cc3a8dac03
Antonio Vallez <nekotony@outlook.com>
2020-10-09 14:21:53 -0400

    Update flask.py

--- flask.py
+++ flask.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.secret_key = 'thisisakey9112'
+    app.run(debug=True, port=9000, threaded=True)
