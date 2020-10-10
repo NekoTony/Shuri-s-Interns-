e957b96ad9e32c6a0069aa40c51ee29580340a96
Antonio Vallez <nekotony@outlook.com>
2020-10-09 20:09:04 -0400

    eeee

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, request, jsonify, url_for, Markup
 import requests
+import json
+from random import choice
+from string import digits
+from qrtools.qrtools import QR
 app = Flask(__name__)
+#app = Flask(static_folder='static')
 
-partner_id = "partnerId = ptnr_1lzs0zD6uWo7k-OdUXjLW_pFgX3"
-PaymentTransferJson = {}
 
 @app.route("/")
 def home():
@@ -31,17 +34,30 @@ def check():
 
 @app.route("/checkout")
 def checkout():
-    PaymentTransfer()
-    return "Template for verification is successful"
+    return PaymentTransfer()
 
 #@app.route("/merchantPaymentTransfer")
 def PaymentTransfer():
-    if PaymentTransferJson == {}:
-        url = "https://sandbox.api.mastercard.com/send/static/v1/partners/{}/merchant/transfers/payment".format(partner_id)
-        r = requests.get(url)
-        print(r.status_code)
+    url = "http://127.0.0.1:8080/merchantPaymentTransfer"
+    headers = {'Content-type': 'application/json'}
+    data = return_json("paymenttransfer.json")
+    data["merchant_transfer"]["transfer_reference"] = "ref_{}".format(''.join(choice(digits) for i in range(10)))
+    #print("------\n{}\n-------".format(data["merchant_transfer"]["transfer_reference"]))
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    QR_data = QRCode("static/qrcode.png")
+    data = r.json()
 
+    return render_template("checkout.html", json=data, qr=QR_data) #https://files.slack.com/files-pri/T019WDZ0A5T-F01C7PJD2F4/qr_sample_for_mastercardqr.png
 
+def return_json(file):
+    with open(file) as f:
+        data = json.load(f)
+    return data
+
+def QRCode(file):
+    qr = qrtools.QR()
+    qr.decode(file)
+    return my_QR.data
 
 if __name__ == "__main__":
     app.run(debug=True)
